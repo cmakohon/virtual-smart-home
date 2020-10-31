b62b0eff99a61ed4c88935f2c781bd2ddfd839fb
cmakohon-captech <cmakohon@captechventures.com>
2020-10-31 12:18:11 -0400

    finished home creator page

--- home-creator/src/App.js
+++ home-creator/src/App.js
@@ -1,23 +1,89 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { AiOutlineHome, AiFillGithub, AiOutlineCopy } from "react-icons/ai";
+import RingLoader from "react-spinners/RingLoader";
 import "./App.css";
 
 function App() {
+  const url =
+    "https://us-central1-virtual-smart-home-8c6b8.cloudfunctions.net/createHome?name=";
   const { register, handleSubmit } = useForm();
+  const [id, setId] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const onSubmit = (data) => {
-    fetch("https://us-central1-virtual-smart-home-8c6b8.cloudfunctions.net/createHome?name=" + data.homeName)
-      .then(response=>response.json())
-      .then(response => {
-        console.log(response.result);
+    setLoading(true);
+    fetch(url + encodeURIComponent(data.homeName))
+      .then((response) => response.json())
+      .then((response) => {
+        setId(response.result);
+        setLoading(false);
       })
-      .catch((err) => {});
+      .catch((err) => setLoading(false));
   };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(id).then(function() {
+      //copy success
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
+
   return (
     <div className="App">
-      <h1>Create your home</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="homeName" ref={register} />
-        <input type="submit" />
-      </form>
+      <div className="container">
+        <h1>
+          <AiOutlineHome
+            style={{
+              marginRight: "1.5rem",
+              fontSize: "36pt",
+              marginBottom: "-8px",
+            }}
+          />
+          Create your home
+        </h1>
+        {!loading && !id && (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="homeName"
+              ref={register}
+              placeholder="Home name"
+              required
+            />
+            <input type="submit" />
+          </form>
+        )}
+        {loading && (
+          <RingLoader size={42} color={"#92DCE5"} loading={loading} />
+        )}
+        {!loading && id && (
+          <div className="result">
+            <span className="result-id">{id}</span>
+            <AiOutlineCopy
+              onClick={onCopy}
+              color={'#92DCE5'}
+              style={{
+                marginLeft: "1rem",
+                fontSize: "16pt",
+                marginBottom: "-4px",
+                cursor: 'pointer'
+              }}
+          />
+          </div>
+        )}
+      </div>
+      <a href="https://github.com/cmakohon" className="footer">
+        <AiFillGithub
+          style={{
+            marginRight: ".5rem",
+            fontSize: "16pt",
+            marginBottom: "-4px",
+          }}
+        />
+        github.com/cmakohon
+      </a>
     </div>
   );
 }
