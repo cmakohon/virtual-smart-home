85b61f74ad02bec56cf3db8ec94f421464d344f9
cmakohon-captech <cmakohon@captechventures.com>
2020-10-30 21:00:10 -0400

    frontend working with CORS

--- functions/index.js
+++ functions/index.js
@@ -28,78 +28,73 @@ const defaultDevices = [
 ];
 
 exports.createHome = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => {
-    const name = req.query.name;
-    // Push the new home into Cloud Firestore using the Firebase Admin SDK.
-    defaultDevices.forEach(d => d.id = uuidv4());
-    const homeResult = await admin.firestore().collection('homes').add({
-      name: name,
-      devices: defaultDevices
-    });
-    // Send back a message that we've succesfully created the home
-    res.json({result: homeResult.id});
+  cors(req, res, () => { });
+  const name = req.query.name;
+  // Push the new home into Cloud Firestore using the Firebase Admin SDK.
+  defaultDevices.forEach(d => d.id = uuidv4());
+  const homeResult = await admin.firestore().collection('homes').add({
+    name: name,
+    devices: defaultDevices
   });
+  // Send back a message that we've succesfully created the home
+  res.json({result: homeResult.id});
 });
 
 exports.getHome = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => { 
-    const homeId = req.query.id;
-    // Push the new home into Cloud Firestore using the Firebase Admin SDK.
-    const readResult = await admin.firestore().collection('homes').doc(homeId).get();
-    // Send back a message that we've succesfully created the home
-    res.json({result: readResult.data()});
-  });
+  cors(req, res, () => { });
+  const homeId = req.query.id;
+  // Push the new home into Cloud Firestore using the Firebase Admin SDK.
+  const readResult = await admin.firestore().collection('homes').doc(homeId).get();
+  // Send back a message that we've succesfully created the home
+  res.json({result: readResult.data()});
 });
 
 exports.addDevice = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => { 
-    const homeId = req.query.id;
-    let device = req.body.device;
-    device.id = uuidv4();
-    
-    // Push the new home into Cloud Firestore using the Firebase Admin SDK.
-    const homeRef = admin.firestore().collection('homes').doc(homeId);
-    const updateResult = await homeRef.update({
-      devices: admin.firestore.FieldValue.arrayUnion(device)
-    });
-    // Send back a message that we've succesfully created the home
-    res.json({result: updateResult});
+  cors(req, res, () => { });
+  const homeId = req.query.id;
+  let device = req.body.device;
+  device.id = uuidv4();
+  
+  // Push the new home into Cloud Firestore using the Firebase Admin SDK.
+  const homeRef = admin.firestore().collection('homes').doc(homeId);
+  const updateResult = await homeRef.update({
+    devices: admin.firestore.FieldValue.arrayUnion(device)
   });
+  // Send back a message that we've succesfully created the home
+  res.json({result: updateResult});
 });
 
 exports.removeDevice = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => { 
-    const homeId = req.query.id;
-    let device = req.body.device;
-  
-    const homeRef = admin.firestore().collection('homes').doc(homeId);
-    const updateResult = await homeRef.update({
-      devices: admin.firestore.FieldValue.arrayRemove(device)
-    });
-    // Send back a message that we've succesfully removed the device
-    res.json({result: updateResult});
+  cors(req, res, () => { });
+  const homeId = req.query.id;
+  let device = req.body.device;
+
+  const homeRef = admin.firestore().collection('homes').doc(homeId);
+  const updateResult = await homeRef.update({
+    devices: admin.firestore.FieldValue.arrayRemove(device)
   });
+  // Send back a message that we've succesfully removed the home
+  res.json({result: updateResult});
 });
 
 exports.updateDeviceState = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => { 
-    const homeId = req.query.id;
-    const update = req.body.update;
+  cors(req, res, () => { });
+  const homeId = req.query.id;
+  const update = req.body.update;
 
-    const homeRef = admin.firestore().collection('homes').doc(homeId);
-    const homeResult = await homeRef.get();
-    let devices = homeResult.data().devices;
-    devices.forEach(d => {
-      if (d.id === update.id) {
-        d.state = update.state;
-      }
-    });
-    const updateResult = await homeRef.update({
-      devices: devices
-    });
-    // Send back a message that we've succesfully updated the device
-    res.json({result: updateResult});
+  const homeRef = admin.firestore().collection('homes').doc(homeId);
+  const homeResult = await homeRef.get();
+  let devices = homeResult.data().devices;
+  devices.forEach(d => {
+    if (d.id === update.id) {
+      d.state = update.state;
+    }
   });
+  const updateResult = await homeRef.update({
+    devices: devices
+  });
+  // Send back a message that we've succesfully updated the device
+  res.json({result: updateResult});
 });
 
 
